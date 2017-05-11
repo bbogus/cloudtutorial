e6b64e45d66edd273b2151ccdd157a110d389b5c
BBogus <jhjh1006@naver.com>
2017-05-11 11:51:10 +0900

    adding files

---
+++ MongoDB_NodeJS_Tutorial/app.js
@@ -0,0 +1,73 @@
+var fs = require('fs');
+var ejs = require('ejs');
+var mongojs = require('mongojs');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var client = mongojs('Company',['products']);
+
+var app = express();
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.listen(52273, function () {
+	console.log('server running at http://127.0.0.1:52273');
+});
+
+app.get('/', function (request, response) {
+	fs.readFile('list.html', 'utf8', function(error,data){
+	client.products.find({},function (error, results){
+	response.send(ejs.render(data, {
+		data: results
+				})); 
+			}); 
+		}); 
+	});
+
+app.get('/delete/:id', function (request, response) {
+		client.query('DELETE FROM products WHERE id=?', [request.params.id], function () {
+			response.redirect('/');
+		});
+	});
+
+app.get('/insert', function (request, response) {
+	fs.readFile('insert.html', 'utf8', function (error, data){
+		response.send(data);
+	});
+});
+
+app.post('/insert',function(request, response){
+	var body = request.body;
+	
+	client.query('INSERT INTO products (name, modelnumber, series) VALUES (?, ?, ?)', 
+				[body.name, body.modelnumber, body.series], 
+			function() {
+			response.redirect('/');
+	});	
+});
+app.get('/edit/:id',function(request, response){
+	fs.readFile('edit.html', 'utf8', function (error, data) {
+	client.query('SELECT * FROM products WHERE id = ?',
+				[request.params.id],
+				function (error, result){
+				response.send(ejs.render(data, {
+					data: result[0]
+				}));
+		});
+	});
+});
+app.post('/edit/:id',function(request, response){
+	var body = request.body;
+	client.query('UPDATE products SET name=?, modelnumber=?, series=? WHERE id=?',
+				[body.name, body.modelnumber, body.series, request.params.id],
+				function() {
+				response.redirect('/');
+		});
+});
+
+
+
+
+  
+	
